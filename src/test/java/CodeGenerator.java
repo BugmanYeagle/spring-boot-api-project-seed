e0bcb7f9be850e15a6b74cd4746a024c29bc77d7
BugmanYeagle <you@example.com>
2019-05-19 12:26:17 +0800

    数据源更改

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -18,10 +18,10 @@ import static com.company.project.core.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/supervision?serverTimezone=UTC&useUnicode=true&characterEncoding=utf8";
     private static final String JDBC_USERNAME = "root";
-    private static final String JDBC_PASSWORD = "123456";
-    private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+    private static final String JDBC_PASSWORD = "10086";
+    private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
 
     private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
     private static final String TEMPLATE_FILE_PATH = PROJECT_PATH + "/src/test/resources/generator/template";//模板位置
@@ -37,7 +37,7 @@ public class CodeGenerator {
     private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
 
     public static void main(String[] args) {
-        genCode("输入表名");
+        genCode("enterprise");
         //genCodeByCustomModelName("输入表名","输入自定义Model名称");
     }
 
